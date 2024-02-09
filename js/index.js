1da8316c9b2028b356a21d50d84dffaa19ccf003
Sergio Henrique de Souza Bezerra <sergio.souzabezerra@outlook.com>
2024-02-09 11:01:20 -0300

    pré-pronto

--- js/index.js
+++ js/index.js
@@ -1,26 +1,63 @@
 let nome
-let win
-let loser 
-var calcular
+let pushFuncition1
+let pushFuncition2
+let pushFuncition3
 
-nome=prompt("Informe o nome do heroi: ")
-console.log(nome)
+do{
 
+    nome =prompt("Informe seu nome de herói:")
 
-calculoFinal()
+    pushFuncition1=getName(nome," ")
+    
+    function getName(nome,splitChar="-"){
+        let getFirstName=nome.split(splitChar)[0]
+        console.log(getFirstName)
+        return getFirstName
+    }
+    
+    pushFuncition2=getCal(pushFuncition1)
+    
+    function getCal(nome){
+        let v = prompt("Herói de nome "+nome+" . informe suas vitórias durante essa longa jornada")
+        let d = prompt(nome+" , informe suas derrotas durante essa longa jornada")
+        let cal=v-d
+        return cal
+    }
+    
+    pushFuncition3=getRank(pushFuncition1,pushFuncition2)
+    
+    function getRank(nome,rank){
+        if(rank<=10){
+            console.log("Herói de nome "+nome+" , aprtir dos seus dados de lutas você está no FERRO")
+        }
+    
+        else if(rank<=20){
+            console.log("Herói de nome "+nome+" , aprtir dos seus dados de lutas você está no BRONZE")
+        }
+    
+        else if(rank<=50){
+            console.log("Herói de nome "+nome+" , aprtir dos seus dados de lutas você está no PRATA")
+        }
+    
+        else if(rank<=80){
+            console.log("Herói de nome "+nome+" , aprtir dos seus dados de lutas você está no OURO")
+        }
+    
+        else if(rank<=90){
+            console.log("Herói de nome "+nome+" , aprtir dos seus dados de lutas você está no DIAMANTE")
+        }
+    
+        else if(rank<=100){
+            console.log("Herói de nome "+nome+" , aprtir dos seus dados de lutas você está no LENDÁRIO")
+        }
+    
+        else {
+            console.log("Herói de nome "+nome+" , aprtir dos seus dados de lutas você está no IMORTAL")
+        }
+    
 
-
-function calculoFinal(){
-    win=prompt("Heroi/Heroína com o nome de "+nome+" , quantas vitórias você possui")
-    console.log(win)
-
-    loser=prompt("Heroi/Heroína com o nome de "+nome+" , quantas derrotas você possui")
-    console.log(loser)
-
-    calcular=win-loser
-
-    alert(calcular)
-}
-
-
+    }
+    
+repetir=prompt("Se sim digite S ou s\nSe não digite qualquer coisa")
+}while(repetir=="s"||repetir=="S")
 
